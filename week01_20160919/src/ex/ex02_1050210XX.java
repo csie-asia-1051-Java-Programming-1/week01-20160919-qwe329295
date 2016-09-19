4995e88c22c2468d8945153dd42e2f3072148c13
蕭佑誠 <105021028@live.asia.edu.tw>
2016-09-19 21:38:02 +0800

    ex繳交

--- week01_20160919/src/ex/ex02_1050210XX.java
+++ week01_20160919/src/ex/ex02_1050210XX.java
@@ -2,13 +2,19 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入體重單位 "公斤"，轉換成 "磅" (1磅 = 0.45359 公斤)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021028 蕭佑誠
  */
-
-public class ex02_1050210XX {
+ import java.util.Scanner;
+public class ex02_1050210XX{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("輸入公斤數");
+		Scanner scn = new Scanner(System.in);
+		double a = scn.nextDouble();
+		double b = 0;
+		b = a*0.45359;
+		System.out.println(b);
 
 	}
 
--- week01_20160919/src/ex/ex03_1050210XX.java
+++ week01_20160919/src/ex/ex03_1050210XX.java
@@ -2,13 +2,19 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入數字 a 與 b，程式 計算 a 的 b 次方後輸出結果 (請使用 Math.pow(a,b) 工具)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021028 蕭佑誠
  */
-
+ import java.util.Scanner;
 public class ex03_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("輸入a與b");
+		Scanner scn = new Scanner(System.in);
+		double a = scn . nextDouble();
+		double b = scn . nextDouble();
+		double c = (Math.pow(a,b));
+		System.out.println(c);
 
 	}
 

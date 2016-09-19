55fd22614ab6036a86cbfdf2a7467cac5ee86eb6
蕭佑誠 <105021028@live.asia.edu.tw>
2016-09-19 16:05:48 +0800

    ex01繳交

--- week01_20160919/src/ex/ex01_1050210XX.java
+++ week01_20160919/src/ex/ex01_1050210XX.java
@@ -2,14 +2,23 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入「華氏」溫度，輸出「攝氏」溫度 (華氏 = 攝氏*(9/5)+32)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021028 蕭佑誠
  */
-
-public class ex01_1050210XX {
+import java.util.Scanner;
+public class ex01_1050210xx {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("請輸入華氏溫度:");
+		Scanner scn =new Scanner(System.in);
+		double a = scn.nextDouble();
+		
+		
+		double b = 0;
+		
+		
+		b=(a-32)*5/9;
+		System.out.println(b);
 	}
 
 }

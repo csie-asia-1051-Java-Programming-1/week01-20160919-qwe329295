e9c949e9b2708c7b668fa843a3527a95fa472bb8
蕭佑誠 <105021028@live.asia.edu.tw>
2016-09-23 20:06:42 +0800

    hw01繳交

--- week01_20160919/src/hw/hw01_1050210XX.java
+++ week01_20160919/src/hw/hw01_1050210XX.java
@@ -1,9 +1,15 @@
 package hw;
-
+import java.util.Scanner;
 public class hw01_1050210XX {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		System.out.println("輸入n");
+		int n =scn.nextInt();
+		
+		Math.ceil(Math.log(n)/Math.log(2));
+		System.out.println(Math.ceil(Math.log(n)/Math.log(2)));
 
 	}
 
